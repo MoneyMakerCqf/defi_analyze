319dabd57eaaca013dbd3103896dac3c8d92bed3
MoneyMakerCqf <740459762@qq.com>
2021-04-08 16:12:56 +0800

    修改了导航栏，底栏的设计

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,33 @@
 <template>
-  <div id="navi">
-    <el-row :gutter="10" type="flex" style="height: 100%">
-      <el-col :span="3">
-        <router-link to='/' class="el-col">
-          <el-image
-            style="width: 50px; height: 50px;"
-            :src="require('../assets/logo.png')"
-            :fit="fit">
-          </el-image>
-          <p style="color:#000000;">Defi-Analyze</p>
-        </router-link>
-      </el-col>
+  <div id="nav">
+    <router-link to='/' style="width: 25%">
+      <div class="div_logo">
+        <el-image
+          style="width: 50px; height: 50px;"
+          :src="require('../assets/logo.png')"
+          :fit="fit">
+        </el-image>
+        <p class="p_logo">Defi-Analyze</p>
+      </div>
+    </router-link>
 
-    </el-row>
+    <router-link to='/' style="width: fit-content">
+      <p class="p_link">Home</p>
+    </router-link>
+
+    <router-link to='/about' style="width: fit-content">
+      <p class="p_link">About us</p>
+    </router-link>
+
+    <a href='https://github.com/MoneyMakerCqf/defi_analyze/tree/main' style="width: fit-content" target="_blank">
+      <div class="div_logo">
+        <el-image
+            style="width: 60px; height: 60px;"
+            :src="require('../assets/github_tag.png')"
+            :fit="fit">
+        </el-image>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -27,17 +42,57 @@ export default {
   methods: {
 
   }
-}
+};
 </script>
 
 <style scoped>
-#navi {
-  height: 60px;
+#nav {
+  display: grid;
+  grid-template-columns: 70%;
+  grid-auto-flow: column;
+  gap: 0.5em;
+  margin-left: 1%;
 }
-.el-col {
-  display: inline-flex;
-  justify-content: center;
+.div_logo {
+  display: flex;
   align-items: center;
-  height: 100%;
+  height: 60px;
+  margin-left: 5%;
+}
+.p_logo {
+  color: #303030;
+  font-size: 25px;
+  display: inline;
+}
+.p_link {
+  position: relative;
+  color: #303030;
+  font-size: 25px;
+  transition: color .3s ease;
+  display: inline-block;
+  margin-block-start: 0;
+  margin-block-end: 0;
+}
+.p_link::after {
+  --scale: 0;
+  --x: 0;
+  content: '';
+  height: 4px;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background: #4c81c9;
+  -webkit-transform: scaleX(var(--scale));
+  transform: scaleX(var(--scale));
+  -webkit-transform-origin: var(--x) 50%;
+  transform-origin: var(--x) 50%;
+  transition: transform 0.3s cubic-bezier(0.535, 0.05, 0.355, 1), -webkit-transform 0.3s cubic-bezier(0.535, 0.05, 0.355, 1);
+}
+.p_link:hover {
+  color: #4c81c9;
+}
+.p_link:hover::after{
+  --scale: 1;
 }
 </style>
---
+++ src/components/foot.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="navi">
+    <el-row :gutter="20" type="flex" style="height: 100%">
+      <el-col :span="6">
+        <p> Copyright by Sustech-Defi-Analyze-Group </p>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+name: "foot"
+}
+</script>
+
+<style scoped>
+#navi {
+  height: 60px;
+}
+.el-col {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+el-col:hover{
+  color: #6495ed;
+}
+</style>
